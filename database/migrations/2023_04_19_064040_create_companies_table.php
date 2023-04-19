bb523f9bcc543e1a19bd00198c717f13d8c78aed
DanielReitz <d@r.de>
2023-04-19 10:13:41 +0200

    2039 - companies-Migration
    Migration für companies table ausgefüllt

--- database/migrations/2023_04_19_064040_create_companies_table.php
+++ database/migrations/2023_04_19_064040_create_companies_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('name');
+            $table->string('location');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('website');
             $table->timestamps();
         });
     }
