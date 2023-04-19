b503d7e1b162615e697e677d15ee2ef991a7fc26
DanielReitz <d@r.de>
2023-04-19 13:30:51 +0200

    2045 - Image Model ausfüllen
    -protected fillables
    -validationRules
    -polymorphic relationship zu Job/User/Company

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Image extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'path',
+        'imageable_id',
+        'imageable_type',
+    ];
+
+    public function validationRules(): array
+    {
+        return [
+            'path' => 'required',
+            'imageable_id' => 'required',
+            'imageable_type' => 'required',
+        ];
+    }
+
+    // Relationship to imageable models (Job, User, Company)
+    public function imageable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+
 }
