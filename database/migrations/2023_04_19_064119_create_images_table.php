eea247d6db059b98d8145c733c8917d3b4b3a281
DanielReitz <d@r.de>
2023-04-20 15:10:07 +0200

    2098 - images-Migration korrigieren
    -imageable_id und imageable_type Spalten ersetzt durch morphs('imageable')

--- database/migrations/2023_04_19_064119_create_images_table.php
+++ database/migrations/2023_04_19_064119_create_images_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,11 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+
             $table->string('path');
-            $table->integer('imageable_id');
-            $table->string('imageable_type');
+
+            $table->morphs('imageable');
+
             $table->timestamps();
         });
     }
