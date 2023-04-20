9c4d7e5b3ef1ccf5658490a0c59e883d1a73d629
DanielReitz <d@r.de>
2023-04-20 14:37:33 +0200

    2094 - Korrektur companies Migration
    -nullable fields
    -split up location into 3 columns
    -replaced manual foreignId with foreignIdFor User

--- database/migrations/2023_04_19_064040_create_companies_table.php
+++ database/migrations/2023_04_19_064040_create_companies_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,11 +12,18 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+
             $table->string('name');
-            $table->string('email');
-            $table->string('location');
-            $table->string('website');
+
+            $table->string('email')->nullable();
+            $table->string('website')->nullable();
+
+            $table->string('street')->nullable();
+            $table->string('postcode')->nullable();
+            $table->string('city')->nullable();
+
+            $table->foreignIdFor(\App\Models\User::class);
+
             $table->timestamps();
         });
     }
