6641d4f3ca8c6173b99facd0c5209f745901a49e
DanielReitz <d@r.de>
2023-04-20 15:06:30 +0200

    2097 - jobs-Migration ändern
    -überflüssige spalten entfernt (location, email, website)
    -nullables
    -manuelle foreignId ersetzt durch foreignIdFor Company

--- database/migrations/2023_04_19_064109_create_jobs_table.php
+++ database/migrations/2023_04_19_064109_create_jobs_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,12 +12,12 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+
             $table->string('title');
-            $table->string('location');
-            $table->string('email');
-            $table->string('website');
-            $table->longText('description');
+            $table->longText('description')->nullable();
+
+            $table->foreignIdFor(\App\Models\Company::class);
+
             $table->timestamps();
         });
     }
