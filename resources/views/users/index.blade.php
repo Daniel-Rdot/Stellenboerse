b97676a03aa0c993b66624e316c1be58ce32d9bf
DanielReitz <d@r.de>
2023-04-24 16:57:23 +0200

    2064 - Listenansicht User ausfüllen
    -users.index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,8 +1,11 @@
 @include('app')
+
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
     @forelse ($users as $user)
         <x-user-card :user="$user"/>
     @empty
         {{ trans('app.no_users') }}
     @endforelse
+
 </div>
