689d8fcac24ffda226f192c88f07edb4b84353c4
DanielReitz <d@r.de>
2023-04-26 09:56:26 +0200

    2121 - Register
    -Register View designt
    -Validation Rules im RegisterController

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,72 @@
-@extends('layouts.app')
+@extends('app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <x-back/>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <div class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24">
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">
+                {{ trans('app.register') }}
+            </h2>
+            <p class="mb-4">{{ trans('app.new_acc') }}</p>
+        </header>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
+            <div class="mb-6">
+                <label for="email"
+                       class="inline-block text-lg mb-2">{{ trans('app.email_address') }}</label>
+
+
+                <input id="email" type="email"
+                       class="border border-gray-200 rounded p-2 w-full
+                       form-control @error('email') is-invalid @enderror" name="email"
+                       value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+
+            </div>
+
+            <div class="mb-6">
+                <label for="password"
+                       class="inline-block text-lg mb-2">{{ trans('app.password') }}</label>
+
+                <div class="col-md-6">
+                    <input id="password" type="password"
+                           class="border border-gray-200 rounded p-2 w-full
+                           form-control @error('password') is-invalid @enderror" name="password"
+                           required autocomplete="new-password">
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    @enderror
                 </div>
             </div>
-        </div>
+
+            <div class="mb-6">
+                <label for="password-confirm"
+                       class="inline-block text-lg mb-2">{{ trans('app.password_confirmation') }}</label>
+
+                <div class="col-md-6">
+                    <input id="password-confirm" type="password" class="border border-gray-200 rounded p-2 w-full
+                    form-control" name="password_confirmation" required autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="mb-6">
+                <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+                    {{ trans('app.sign_up') }}
+                </button>
+            </div>
+        </form>
     </div>
-</div>
+
 @endsection
